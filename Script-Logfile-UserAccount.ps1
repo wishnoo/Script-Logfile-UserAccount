891e1909124b25a19e82346351a3926609aeebbd
wishnoo <rwishnoo@gmail.com>
2020-07-15 19:13:11 -0400

    Hashtable with foreach

--- Script-Logfile-UserAccount.ps1
+++ Script-Logfile-UserAccount.ps1
@@ -1,11 +1,19 @@
-$temp = net user STEALTH
-$temp | ForEach-Object {
-    if ($_ | findstr /c:"Password expires" | findstr /c:"Never"){
-        if ($_ | findstr /c:"Never") {
-            Write-Verbose "Password Never Expires" -Verbose
-        }
-        else {
-            Write-Verbose "Password Does Expire" -Verbose
-        }
+<#
+netUserProperty_with_expectedvalue is a hashtable to store the netuser properties that need to be verified as the key and desired values as the values in the hashtable.
+#>
+$netUserProperty_with_expectedvalue = @{'Account active' = 'Yes'
+                            'Password expires' = 'Never'
+                            'User may change password' = 'Yes'
+                            'Wishnoo' = 'Yes'
+}
+
+$netuserobject = net user STEALTH
+<#
+We iterate through the hashtable and find the key in the netuserobject and futher find the expected value.
+This is then logged in to the verbose stream and log file.
+#>
+foreach ($key in $netUserProperty_with_expectedvalue.Keys) {
+    if ( $netuserobject | findstr /c:"$($key)" | findstr /c:"$($netUserProperty_with_expectedvalue.$key)") {
+        Write-Verbose "$($key) - $($netUserProperty_with_expectedvalue.$key)" -Verbose
     }
 }
