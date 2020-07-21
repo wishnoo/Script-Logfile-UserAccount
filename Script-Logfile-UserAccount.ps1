01236f210ccaf5f65bc52daca1dca826b8d2cf5d
wishnoo <rwishnoo@gmail.com>
2020-07-21 10:33:07 -0400

    Added functions for log file name and parameter validation

--- Script-Logfile-UserAccount.ps1
+++ Script-Logfile-UserAccount.ps1
@@ -1,3 +1,49 @@
+Submit-Log -text "Start of program"
+
+param (
+    [Parameter()]
+    [string[]]$AccountName
+)
+
+function LogFileName {
+    [cmdletbinding()]
+    param (
+        [switch] $requireComputerName,
+        [string] $fileNamePrefix,
+        [switch] $errorFilePath,
+        [switch] $successFilePath
+    )
+    Write-Verbose "LogFileName function started" -Verbose
+    <#
+    currentFolderPath - Name of the path where the executable reside relative to path of the console.
+    #>
+    $currentFolderPath = Split-Path $script:MyInvocation.MyCommand.Path
+    $currentFolderPath += '\'
+    <#
+    compname - Current local computer name where the script is executed.
+    #>
+    $compName = $env:COMPUTERNAME
+
+    if (($requireComputerName) -and ($fileNamePrefix) ) {
+        $outputFilename = "$($fileNamePrefix)_$($compName).txt"
+        $logFilePath = $currentFolderPath + $outputFilename
+        return $logFilePath
+    }
+    elseif ((-Not $requireComputerName) -and ($fileNamePrefix)) {
+        $outputFilename = "$($fileNamePrefix).txt"
+        $logFilePath = $currentFolderPath + $outputFilename
+        return $logFilePath
+    }
+    elseif ($errorFilePath) {
+        $outputFilename = "$($compName)_ERROR.txt"
+        $logFilePath = $currentFolderPath + $outputFilename
+        return $logFilePath
+    }
+    else {
+        $logFilePath = ""
+        return $logFilePath
+    }
+}
 
 <#
 Log function to output to verbose stream as well as log into file
@@ -8,6 +54,13 @@ function Submit-Log {
         [string]$text,
         $errorRecord
     )
+
+    # logFilePath needs to be assigned beforehand
+    if ((-not $logFilePath)) {
+        $logFilePath = LogFileName -errorFilePath
+        $text = "Log File Path is empty"
+    }
+
     # Prepend time with text using get-date and .tostring method
     $Entry = (Get-Date).ToString( 'M/d/yyyy HH:mm:ss - ' ) + $text
 
@@ -19,13 +72,33 @@ function Submit-Log {
 
     #  If error record included
     #   Recurse to capture exception details
-    If ( $ErrorRecord -is [System.Management.Automation.ErrorRecord] )
+    If ( $errorRecord -is [System.Management.Automation.ErrorRecord] )
     {
-        Submit-Log -Text "Exception.Message [$($ErrorRecord.Exception.Message)]"
-        Submit-Log -Text "Exception.GetType() [$($ErrorRecord.Exception.GetType())]"
-        Submit-Log -Text "Exception.InnerException.Message [$($ErrorRecord.Exception.InnerException.Message)]"
+        Submit-Log -Text "Exception.Message [$($errorRecord.Exception.Message)]"
+        Submit-Log -Text "Exception.GetType() [$($errorRecord.Exception.GetType())]"
+        Submit-Log -Text "Exception.InnerException.Message [$($errorRecord.Exception.InnerException.Message)]"
     }
 }
+
+function ParameterValidation {
+    [cmdletbinding()]
+    param (
+       [string[]] $AccountName
+    )
+    if (-Not $AccountName) {
+        Submit-Log -text "Account Name Array is Empty"
+        exit
+    }
+}
+
+$fileNamePrefix = "UserAccount"
+$logFilePath = LogFileName -requireComputerName -fileNamePrefix $fileNamePrefix
+
+ParameterValidation $AccountName
+
+
+# $netuserobject = net user stealth
+$netuserobject = net user $AccountName
 
 <#
 netUserProperty_with_expectedvalue is a hashtable to store the netuser properties that need to be verified as the key and desired values as the values in the hashtable.
@@ -34,20 +107,7 @@ $netUserProperty_with_expectedvalue = @{'Account active' = 'Yes'
                             'Password expires' = 'Never'
                             'User may change password' = 'Yes'
 }
-<#
-currentFolderPath - Name of the path where the executable reside relative to path of the console.
-#>
-$currentFolderPath = Split-Path $script:MyInvocation.MyCommand.Path
-$currentFolderPath += '\'
-<#
-compname - Current local computer name where the script is executed.
-#>
-$compName = $env:COMPUTERNAME
-$outputFilename = "UserAccount_$($compName).txt"
-$logFilePath = $currentFolderPath + $outputFilename
-$netuserobject = net user STEALTH
 
-Submit-Log -text "Start of program"
 
 Submit-Log -text "Iteration through the fixed hash map started."
 <#
